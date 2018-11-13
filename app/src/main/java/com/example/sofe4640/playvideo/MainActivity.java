9f18ff5056338b74faf83c2d5bea7c80343265b6
AnwarBari <33730047+AnwarBari@users.noreply.github.com>
2018-11-13 18:20:56 -0500

    first commit

---
+++ app/src/main/java/com/example/sofe4640/playvideo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sofe4640.playvideo;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
